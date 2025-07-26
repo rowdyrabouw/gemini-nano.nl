bd8324f41ef6cc5f2d44a3f073367770ca4a30fb
Rowdy Rabouw <hi@rowdy.codes>
2025-07-26 17:22:59 +0200

    chore: refactor propmt audio demo

--- dist/assets/js/prompt.js
+++ dist/assets/js/prompt.js
@@ -3,6 +3,7 @@ const promptStatus = apiSupport.shadowRoot.querySelector("#prompt-status");
 const promptDownloadButton = apiSupport.shadowRoot.querySelector("#prompt-download");
 const promptError = document.querySelector("#prompt-error");
 const promptInfo = document.querySelector("#prompt-info");
+const promptAudioForm = document.querySelector("#prompt-audio-form");
 const promptPoemForm = document.querySelector("#prompt-poem-form");
 const promptOutput = document.querySelector("#prompt-output");
 
@@ -46,6 +47,55 @@ promptDownloadButton.addEventListener("click", async () => {
   });
 });
 
+if (promptAudioForm) {
+  promptAudioForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    if (availability !== "available") {
+      promptError.classList.add("error");
+      promptError.textContent = `Prompt API is ${availability}.`;
+      console.error(`Prompt API is ${availability}.`);
+      return;
+    }
+
+    promptInfo.textContent = "Thinking ...";
+    promptOutput.textContent = "";
+
+    const formData = new FormData(promptAudioForm);
+    const startTime = performance.now();
+
+    const blob = await (await fetch("/assets/audio/neil.mp3")).blob();
+    const arrayBuffer = await blob.arrayBuffer();
+
+    const params = await LanguageModel.params();
+    const session = await LanguageModel.create({
+      expectedInputs: [{ type: "audio" }],
+      temperature: 0.1,
+      topK: params.defaultTopK,
+    });
+
+    const stream = session.promptStreaming([
+      {
+        role: "user",
+        content: [
+          { type: "text", value: formData.get("prompt").trim() },
+          { type: "audio", value: arrayBuffer },
+        ],
+      },
+    ]);
+
+    for await (const chunk of stream) {
+      promptInfo.textContent = "Transcribing ...";
+      promptOutput.textContent += chunk;
+    }
+
+    const endTime = performance.now();
+    const seconds = ((endTime - startTime) / 1000).toFixed(2);
+    promptInfo.textContent = `Done in ${seconds} seconds!`;
+    console.info(`Request took ${seconds} seconds`);
+  });
+}
+
 if (promptPoemForm) {
   promptPoemForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -61,11 +111,6 @@ if (promptPoemForm) {
     promptOutput.textContent = "";
 
     const formData = new FormData(promptPoemForm);
-    const options = {
-      tone: formData.get("tone"),
-      format: formData.get("format"),
-      length: formData.get("length"),
-    };
     const startTime = performance.now();
 
     const session = await LanguageModel.create();
