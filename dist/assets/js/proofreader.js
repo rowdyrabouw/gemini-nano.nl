293fa6c3b159c12105ccade0a0e00ec2431aff40
Rowdy Rabouw <hi@rowdy.codes>
2025-07-27 10:55:32 +0200

    chore: refactor proofreader

--- dist/assets/js/proofreader.js
+++ dist/assets/js/proofreader.js
@@ -1,9 +1,7 @@
-const supportedMessage = "Proofreader API is supported.";
-const statusMessage = "Proofreader API is";
-const notSupportedMessage = "Proofreader API is not supported.";
 const apiSupport = document.querySelector("api-support");
 const proofReaderStatus = apiSupport.shadowRoot.querySelector("#proofreader-status");
 const proofReaderDownloadButton = apiSupport.shadowRoot.querySelector("#proofreader-download");
+const proofReaderInfo = document.querySelector("#proofreader-info");
 const proofReaderForm = document.querySelector("#proofreader-form");
 const proofReaderOutput = document.querySelector("#proofreader-output");
 
@@ -11,17 +9,18 @@ let availability;
 let proofReader;
 
 if ("Proofreader" in self) {
-  console.info(supportedMessage);
+  console.info("Proofreader API is supported.");
   availability = await Proofreader.availability();
-  console.info(`${statusMessage} ${availability}.`);
+  console.info(`Proofreader API is ${availability}.`);
   proofReaderStatus.textContent = availability;
   proofReaderStatus.classList.remove("status-checking");
   proofReaderStatus.classList.add(`status-${availability}`);
+
   if (availability === "downloadable") {
     proofReaderDownloadButton.hidden = false;
   }
 } else {
-  console.error(notSupportedMessage);
+  console.error("Proofreader API is not supported.");
   availability = "not supported";
   proofReaderStatus.textContent = availability;
   proofReaderStatus.classList.remove("status-checking");
@@ -51,22 +50,30 @@ if (proofReaderForm) {
   proofReaderForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    proofReaderOutput.textContent = "Proofreading ...";
+    proofReaderInfo.textContent = "Thinking ...";
+    proofReaderOutput.textContent = "";
 
     const formData = new FormData(proofReaderForm);
     /* options for the proofreader are defined in the specification, but not implemented in Chrome yet */
     /*
-  const options = {
-    includeCorrectionTypes: true,
-    includeCorrectionExplanations: true,
-    expectedInputLanguagues: ["en"],
-    correctionExplanationLanguage: "en",
-  };
-  const proofreader = await Proofreader.create(options);
-  */
+    const options = {
+      includeCorrectionTypes: true,
+      includeCorrectionExplanations: true,
+      expectedInputLanguagues: ["en"],
+      correctionExplanationLanguage: "en",
+    };
+    const proofreader = await Proofreader.create(options);
+    */
+    const startTime = performance.now();
     const proofreader = await Proofreader.create();
+    proofReaderInfo.textContent = "Proofreading ...";
 
-    const corrections = await proofreader.proofread(formData.get("input").trim());
+    const corrections = await proofreader.proofread(formData.get("prompt").trim());
     proofReaderOutput.textContent = corrections.correctedInput;
+
+    const endTime = performance.now();
+    const seconds = ((endTime - startTime) / 1000).toFixed(2);
+    proofReaderInfo.textContent = `Done in ${seconds} seconds!`;
+    console.info(`Request took ${seconds} seconds`);
   });
 }
