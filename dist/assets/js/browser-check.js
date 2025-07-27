648656c7516746d49c0c643af5b6bc9e1bd2b900
Rowdy Rabouw <hi@rowdy.codes>
2025-07-27 12:16:57 +0200

    fix: fix browse rsupport

--- dist/assets/js/browser-check.js
+++ dist/assets/js/browser-check.js
@@ -1,7 +1,9 @@
 const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 if (!isChrome) {
-  const apiSupport = document.querySelector("api-support");
-  apiSupport.style.visibility = "hidden";
+  const header = document.querySelector("header");
+  header.style.display = "none";
+  const main = document.querySelector("main");
+  main.style.paddingBlockStart = "32px";
   const errorText = "Sorry, this website only works in Chrome at the moment.";
   const browserError = document.querySelector("#browser-error");
   browserError.classList.add("error");
